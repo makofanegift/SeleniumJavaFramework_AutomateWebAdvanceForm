eafed789eade4f0df7a6a15f26065d8c5f7503f7
GiftMakofane <gcmakofane@gmail.com>
2026-07-06 21:06:26 +0200

    added NdosiTest and editted the Base class

---
+++ AtomateWebAdvanceForm_Assessment/src/test/java/tests/NdosiTest.java
@@ -0,0 +1,50 @@
+package tests;
+
+import org.testng.annotations.Test;
+import utils.Base;
+
+public class NdosiTest extends Base {
+
+    @Test
+    public void loginPageTest() throws InterruptedException {
+
+        loginPage.clickLoginMainButton();
+        loginPage.enterLoginUsername("gcmakofane@gmail.com");
+        loginPage.enterLoginPassword("Ndosi_TestSite1");
+        loginPage.clickLoginButton();
+        Thread.sleep(4000);
+    }
+
+    @Test(dependsOnMethods = "loginPageTest")
+    public void loginLandingPageTest()  {
+        landingPage.validateLoginPage();
+        landingPage.clickLearnButton();
+        landingPage.clickLearningMaterialsButton();
+
+    }
+    @Test (dependsOnMethods = "loginLandingPageTest")
+    public void webAutomationAdvancedTest () throws InterruptedException {
+        webAutomationAdvanced.clickWebAutomationAdvanced();
+        webAutomationAdvanced.validateInventoryFormSuccessfully();
+        webAutomationAdvanced.selectDeviceTypeFromDropdown();
+        webAutomationAdvanced.selectDeviceBrand();
+        webAutomationAdvanced.selectDeviceStorage();
+        webAutomationAdvanced.validateDeviceUnitPrice();
+        webAutomationAdvanced.selectDeviceColor();
+        webAutomationAdvanced.enterDeviceQty();
+        webAutomationAdvanced.validateSubTotal();
+        webAutomationAdvanced.enterAddress();
+        webAutomationAdvanced.clickNextButton();
+        webAutomationAdvanced.validateOrderPreview();
+        webAutomationAdvanced.selectExpressShipping();
+        webAutomationAdvanced.expressCharges();
+        webAutomationAdvanced.selectWarranty();
+        webAutomationAdvanced.discountCode();
+        webAutomationAdvanced.applyDiscountCode();
+        webAutomationAdvanced.validateSAVE10discount();
+        webAutomationAdvanced.confirmPurchase();
+        webAutomationAdvanced.viewInvoice();
+        webAutomationAdvanced.viewInvoiceDetails();
+    }
+
+}
