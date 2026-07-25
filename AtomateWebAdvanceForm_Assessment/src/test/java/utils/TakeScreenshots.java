d867b78dd974e5a0f17e57e2a44770b06957fa2d
GiftMakofane <gcmakofane@gmail.com>
2026-07-25 14:12:24 +0200

    added screenshots class and method

--- AtomateWebAdvanceForm_Assessment/src/test/java/utils/TakeScreenshots.java
+++ AtomateWebAdvanceForm_Assessment/src/test/java/utils/TakeScreenshots.java
@@ -1,4 +1,28 @@
 package utils;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+
 public class TakeScreenshots {
+
+    private static  final  String screenshotDir = System.getProperty("user.dir") + "/Screenshots";
+
+    public static String takeSnapshot(WebDriver driver, String screenshotName) {
+
+        TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+        File scr = takesScreenshot.getScreenshotAs(OutputType.FILE);
+        File destination = new File(screenshotDir, screenshotName + ".jpeg");
+
+        try{
+            FileUtils.copyFile(scr, destination);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return destination.getAbsolutePath();
+
+    }
 }
